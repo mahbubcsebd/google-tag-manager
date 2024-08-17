70d4a3a31846015cb3908fa501f6918089581a41
Mahbubur Rahman <mahbub.cse.me@gmail.com>
2024-08-17 16:15:06 +0600

    init

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
-import { GoogleTagManager } from '@next/third-parties/google';
+// import Analytics from '@/components/gtmComponent';
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { Suspense } from 'react';
+import Analytics from "./components/gtmComponent";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,18 +14,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-    <GoogleTagManager gtmId="GTM-5WPZ22RR" />
-      <body className={inter.className}>
-      <header>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/product">Product</Link>
-        </nav>
-      </header>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+          {/* <GoogleTagManager gtmId="GTM-5WPZ22RR" /> */}
+          <body className={inter.className}>
+              <Suspense>
+                  <Analytics />
+              </Suspense>
+              <header>
+                  <nav>
+                      <Link href="/">Home</Link>
+                      <Link href="/about">About</Link>
+                      <Link href="/product">Product</Link>
+                  </nav>
+              </header>
+              {children}
+          </body>
+      </html>
   );
 }
